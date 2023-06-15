b3ff9a720b276d54fb2b23150bd6744b95455c10
KevinDavoust <kevin.davoust@orange.fr>
2023-06-15 12:18:06 +0200

    added job entity, crud, fixtures with category references, and added it to job/show and category/show

--- src/DataFixtures/CompanyFixtures.php
+++ src/DataFixtures/CompanyFixtures.php
@@ -5,12 +5,13 @@ namespace App\DataFixtures;
 use App\Entity\Company;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class CompanyFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = \Faker\Factory::create('fr_FR');
+        $faker = Factory::create('fr_FR');
         for ($i = 0; $i < 50; $i++) {
             $company = new Company();
             $company->setName($faker->company)
