42fae5e15c7d893c39fa2e9e52e7e11ad121b58b
KevinDavoust <kevin.davoust@orange.fr>
2023-06-21 16:24:52 +0200

    manque le responsive

--- src/Form/JobofferApplyType.php
+++ src/Form/JobofferApplyType.php
@@ -5,10 +5,16 @@ namespace App\Form;
 use App\Entity\Resume;
 use App\Entity\User;
 use App\Repository\ResumeRepository;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
 
 class JobofferApplyType extends AbstractType
 {
@@ -16,17 +22,42 @@ class JobofferApplyType extends AbstractType
     {
 
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-control mb-3' // Ajoute une classe CSS personnalisée
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'form-control mb-4' // Ajoute une classe CSS personnalisée
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control mb-3' // Ajoute une classe CSS personnalisée
+                ],
+            ])
             ->add('resumes', EntityType::class, [
                 'class' => Resume::class,
                 'choice_label' => 'name',
                 'multiple' => true,
                 'expanded' => false,
                 'placeholder' => 'Choisissez un CV',
+                'label' => 'Choisissez un de vos CV',
+                'constraints' => [
+                    new Count([
+                        'max' => 1,
+                        'maxMessage' => 'Veuillez choisir un seul CV'
+                    ])
+                ],
+                'attr' => [
+                    'class' => 'form-control form-group mb-3 h-auto' // Ajoute une classe CSS personnalisée
+                ],
             ])
-        ;
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
