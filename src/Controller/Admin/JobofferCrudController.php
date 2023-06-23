b509e516fb67dfaccf139b33745af70f4dc3a3c4
KevinDavoust <kevin.davoust@orange.fr>
2023-06-23 11:57:15 +0200

    easyadmin user + joboffer

--- src/Controller/Admin/JobofferCrudController.php
+++ src/Controller/Admin/JobofferCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Joboffer;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class JobofferCrudController extends AbstractCrudController
 {
@@ -12,14 +18,25 @@ class JobofferCrudController extends AbstractCrudController
         return Joboffer::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setPageTitle('index', 'Offres d\'emploi');
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title', 'titre'),
             TextEditorField::new('description'),
-        ];
+            TextField::new('city', 'ville'),
+            AssociationField::new('contract', 'contrat'),
+            DateTimeField::new('createdAt', 'date de création')->hideOnForm()->hideOnIndex(),
+            DateTimeField::new('updatedAt', 'date de modification')->hideOnForm()->hideOnIndex(),
+            AssociationField::new('job', 'emploi'),
+            AssociationField::new('company', 'entreprise'),
+            AssociationField::new('salary', 'salaire min-max')
+            ];
     }
-    */
 }
