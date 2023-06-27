505736bc40384fc9e9bcf91f20e53903ff78891b
Aline LEGENDRE <legendrealine@gmail.com>
2023-06-27 18:52:38 +0200

    added resume controller with function new and resume type

--- src/Form/ResumeType.php
+++ src/Form/ResumeType.php
@@ -4,17 +4,32 @@ namespace App\Form;
 
 use App\Entity\Resume;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ResumeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('path')
-            ->add('user')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de votre CV',
+                'attr' => [
+                    'class' => 'form-control be-form-input mb-md-5 mb-3', // Ajoute une classe CSS personnalisée
+                    'placeholder' => 'CV de développeur web back-end',
+                ],
+            ])
+            ->add('pathFile', VichFileType::class, [
+                'label' => 'CV',
+                'required'      => false,
+                'allow_delete'  => true,
+                'download_uri' => true,
+                'attr' => [
+                    'class' => 'form-control be-form-input mb-3'
+                ],
+            ])
         ;
     }
 
