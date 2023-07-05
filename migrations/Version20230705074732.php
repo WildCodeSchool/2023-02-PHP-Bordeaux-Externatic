628dfe6e4948c9ca10daa31f629f64388a673e72
chado <a.sale@outlook.fr>
2023-07-05 12:05:10 +0200

    fix: update details about css

--- migrations/Version20230705074732.php
+++ migrations/Version20230705074732.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230704112347 extends AbstractMigration
+final class Version20230705074732 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -30,6 +30,7 @@ final class Version20230704112347 extends AbstractMigration
         $this->addSql('CREATE TABLE resume (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(100) DEFAULT NULL, path VARCHAR(255) NOT NULL, INDEX IDX_60C1D0A0A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE salary (id INT AUTO_INCREMENT NOT NULL, min INT NOT NULL, max INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(100) NOT NULL, birthday DATE DEFAULT NULL, city VARCHAR(100) DEFAULT NULL, phone VARCHAR(15) DEFAULT NULL, status INT DEFAULT NULL, is_verified TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE favlist (user_id INT NOT NULL, joboffer_id INT NOT NULL, INDEX IDX_A5BC4835A76ED395 (user_id), INDEX IDX_A5BC4835BD612208 (joboffer_id), PRIMARY KEY(user_id, joboffer_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE company ADD CONSTRAINT FK_4FBF094FA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE job ADD CONSTRAINT FK_FBD8E0F812469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
@@ -41,6 +42,8 @@ final class Version20230704112347 extends AbstractMigration
         $this->addSql('ALTER TABLE joboffer_user ADD CONSTRAINT FK_F947B652A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE resume ADD CONSTRAINT FK_60C1D0A0A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+        $this->addSql('ALTER TABLE favlist ADD CONSTRAINT FK_A5BC4835A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE favlist ADD CONSTRAINT FK_A5BC4835BD612208 FOREIGN KEY (joboffer_id) REFERENCES joboffer (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
@@ -56,6 +59,8 @@ final class Version20230704112347 extends AbstractMigration
         $this->addSql('ALTER TABLE joboffer_user DROP FOREIGN KEY FK_F947B652A76ED395');
         $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('ALTER TABLE resume DROP FOREIGN KEY FK_60C1D0A0A76ED395');
+        $this->addSql('ALTER TABLE favlist DROP FOREIGN KEY FK_A5BC4835A76ED395');
+        $this->addSql('ALTER TABLE favlist DROP FOREIGN KEY FK_A5BC4835BD612208');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE company');
         $this->addSql('DROP TABLE contract');
@@ -66,6 +71,7 @@ final class Version20230704112347 extends AbstractMigration
         $this->addSql('DROP TABLE resume');
         $this->addSql('DROP TABLE salary');
         $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE favlist');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
